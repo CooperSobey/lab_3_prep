6dbd6fe02f1174654eb5746aa127de629246cda5
CooperSobey <csobey@ualberta.ca>
2026-09-14 23:05:19 -0600

    bed time

--- src/UniquePtr.h
+++ src/UniquePtr.h
@@ -1,6 +1,88 @@
 #ifndef UNIQUE_PTR_H
 #define UNIQUE_PTR_H
+#include <algorithm>
 
 // Your implementation here
+template <typename T>
+class UniquePtr {
+    public:
 
+    //ctor/dtor
+    UniquePtr(T *P = nullptr) : mPtr{P} {}
+    ~UniquePtr() {
+        if (mPtr) {
+            delete mPtr;
+        }
+    }
+
+    //Copy Constructor
+    UniquePtr (const UniquePtr& other) = delete;
+    //: mPtr {clone (other.mPtr)} {}
+
+    //Move Constructor
+    UniquePtr (UniquePtr&& other) : mPtr (other.mPtr) {
+        other.mPtr = nullptr;
+    }
+
+    //Copy Assignment
+     UniquePtr& operator=(const UniquePtr& other) = delete;
+    //{
+    //     UniquePtr otherClone(other);
+    //
+    //     std::swap (mPtr, otherClone.mPtr);
+    //     return *this;
+    // }
+
+    //Move Assignment
+    UniquePtr& operator=(UniquePtr&& other) {
+        std::swap (mPtr, other.mPtr);
+        return *this;
+    }
+
+    //Dereference op
+    T& operator*() const { return *mPtr;}
+
+    template<typename U>
+    UniquePtr(UniquePtr<U>&& other) : mPtr(other.release()) {}
+
+
+    //operator->
+    T* operator->() const {return mPtr;}
+
+    //operator==
+    bool operator==(const UniquePtr& other) const {
+        return mPtr == other.mPtr;
+    }
+
+    //T* release()
+    T* release() {
+        T* tmp = mPtr;
+        mPtr = nullptr;
+        return tmp;
+    }
+
+    //T* get()
+    T* get() const {return mPtr;}
+
+    //void reset(T* newPtr = nullptr)
+    void reset(T* newPtr = nullptr) {
+        T* oldPtr = mPtr;
+        mPtr = newPtr;
+        if (oldPtr) {
+            delete oldPtr;
+        }
+
+    }
+
+    //void swap(UniquePtr<t>& other)
+    void swap(UniquePtr<T>& other) {
+        std::swap (mPtr, other.mPtr);
+    }
+
+    // operator bool()
+    operator bool() const {return mPtr != nullptr;}
+
+    private:
+        T* mPtr;
+};
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,37 @@
+#include <iostream>
+#include <ostream>
+
 #include "UniquePtr.h"
+
+void testEqual() {
+    UniquePtr<int> ptr1(new int(7));
+
+    if (ptr1 != nullptr) {
+        std::cout << "Test ptr1 not null pass" << std::endl;
+    }
+}
+
+struct TestArrow {
+    void testArrow() {
+        std::cout << "Test arrow pass" << std::endl;
+    }
+};
 
 int main() {
     // UniquePtr<int> ptr(new int(5));
     // they test all methods here...
+    UniquePtr<int> ptr(new int(5));
+    std::cout << "Test .get " << ptr.get() << std::endl;
+    std::cout << "Test * op "<< *ptr << std::endl;
+    testEqual();
+
+    ptr.reset();
+    if (ptr == nullptr) {
+        std::cout << "Test ptr reset pass" << std::endl;
+    }
+
+    UniquePtr<TestArrow> ptr2(new TestArrow());
+    ptr2->testArrow();
+
     return 0;
 }
